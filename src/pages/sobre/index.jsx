1d1a3418d53f61d396845a23b164886a285b0fae
paulomussi <127447854+paulomussi@users.noreply.github.com>
2026-02-27 22:46:56 -0300

    create projeto vite

---
+++ src/pages/sobre/index.jsx
@@ -0,0 +1,10 @@
+
+export default function Sobre () {
+    return(
+        <>
+            <h1>Sobre</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nulla iure aut expedita tempore nisi eum dolorem modi hic aliquam, quae eveniet provident animi ea magnam a odit ab in.</p>
+        </>
+    )
+}
