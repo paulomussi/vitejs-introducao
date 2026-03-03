bb4ebe0911e31f46b2a69c9e308046284e95ddd6
paulomussi <127447854+paulomussi@users.noreply.github.com>
2026-03-02 22:45:19 -0300

    organizacao-do-projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-import reactDOM from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+
+import App from './App.jsx'
 
 import './estilos/reset.css'
 import './estilos/globals.css'
-import App from './App.jsx'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,17 @@
+
+import './index.css'
+
+function Home() {
+  return (
+    <>
+    <h1>Aula introdução Vite JS</h1>
+    <h2>Preparo do projeto</h2>
+    <h3>Estilização</h3>
+    <p>Entendendo uso de css dentro do projeto.</p>
+    <label> Exemplo label </label>
+    <small> Exemplo label </small>
+    </>
+  )
+}
+
+export default Home
