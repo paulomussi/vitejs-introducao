ce2993fbda50ef29a299305780bf9a5bea074e20
paulomussi <127447854+paulomussi@users.noreply.github.com>
2026-03-16 22:49:24 -0300

    d

---
+++ src/pages/atividades/atv-01/index.jsx
@@ -0,0 +1,28 @@
+
+import styles from './index.module.css';
+
+function Atividade01 () {
+    return(
+        <>
+            <h1 className={styles.titulo}>São Paulo Maior do Brasil</h1>
+            
+            <div className = {styles.div}>
+
+            <div className = {styles.vermelho}>
+              
+            </div>
+
+            <div className = {styles.preto}>
+               
+            </div>
+
+            <div className = {styles.branco}>
+              
+            </div>
+                
+            </div>
+        </>
+    )
+}
+
+export default Atividade01
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,9 +8,12 @@ function Home() {
         <h1>Exemplos</h1>
         <Link to="/exemplo/1">Exemplo 1</Link>
         <Link to="/exemplo/2">Exemplo 2</Link>
+        <Link to="/exemplo/3">Exemplo 3</Link>
       </div>  
       <div className={styles.containerLista}>
         <h1>Atividades</h1>
+        <Link to="/atividade/1">Atividade 1</Link>
+        <Link to="/atividade/2">Atividade 2</Link>
       </div>
     </div>
   )
