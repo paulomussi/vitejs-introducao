0f1dc53171e4560009d9e3d6129dc18eb68bdb19
paulomussi <127447854+paulomussi@users.noreply.github.com>
2026-03-23 22:11:43 -0300

    d

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './App.jsx'
+import App from './App.jsx';
 
-import './estilos/reset.css'
-import './estilos/globals.css'
+import './estilos/reset.css';
+import './estilos/globals.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
--- src/pages/exemplos/ex-01/index.jsx
+++ src/pages/exemplos/ex-01/index.jsx
@@ -2,8 +2,8 @@
 export default function Exemplo01 () {
     return(
         <>
-            <h1>Exemplo 01</h1>
-
+            <h1>Exemplo 1</h1>
+            
             <p>Componente mínimo</p>
         </>
     )
